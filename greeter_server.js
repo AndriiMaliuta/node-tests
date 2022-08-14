4b12fbeea1f35a5402136c149d34bdf42cb180a4
malandr <quadr988@gmail.com>
2022-08-14 19:36:59 +0300

    init

--- greeter_server.js
+++ greeter_server.js
@@ -1,3 +1,5 @@
+const grpc = require('@grpc/grpc-js');
+
 function sayHello(call, callback) {
     callback(null, {message: 'Hello ' + call.request.name});
 }
@@ -6,3 +8,11 @@ function sayHelloAgain(call, callback) {
     callback(null, {message: 'Hello again, ' + call.request.name});
 }
 
+function main() {
+    let server = new grpc.Server();
+    server.addService(hello_proto.Greeter.service,
+        {sayHello: sayHello, sayHelloAgain: sayHelloAgain});
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+    });
+}
